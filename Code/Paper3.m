558ba6681d03f2675b55a76b7c0dc1333a11e0f5
Orazio <orazioalibra@gmail.com>
2026-04-24 13:48:44 +0200

    Updates, finished ODEs, PDE missing

--- Code/Paper3.m
+++ Code/Paper3.m
@@ -38,10 +38,10 @@ odefun= @(t,x) [alpha*x(1)*(1-(x(1)/Tc))-h*((lambda_K*x(1)*x(2)+lambda_E*x(1)*x(
     rE*x(3)-delta_E*x(4);...
     rho_K*x(2)+rho_B*x(3)-h*x(5)*(x(2)+x(3))-delta_P*x(5)];
 
-x0triv=[0,0,0,0,0];
-x0=[1,2*1e-4,0,0,0];
+%x0triv=[0,0,0,0,0];
+x0=[3,2*1e-4,0,0,0];
 
-[t,x]=ode23(odefun,tspan, x0);
+[~,x]=ode23(odefun,tspan, x0);
 
 figure
 subplot(5,1,1); plot(tspan, x(:,1)); title('N'); xlabel('Days'); ylabel('Tumor');
@@ -63,19 +63,19 @@ drawnow;
 % E_1=x(7) Deeply-Exausted T cells                       per k per mm^2
 
 
-lambdas=[lambda_E,lambda_E,lambda_E];
-deltas=[delta_E,delta_E,delta_E];
-r_t=5*rE; %fianda
+lambday=[lambda_E,lambda_E,lambda_E];
+deltay=[delta_E,delta_E,delta_E];
+r_t=5*rE;
 
-odefun2= @(t,x) [alpha*x(1)*(1-(x(1)/Tc))-h*((lambda_K*x(1)*x(2)+lambdas*x(1)*[x(4);x(6);x(7)])/(x(1)+x(2)+x(4)+x(6)+x(7)+1));...
+odefun2= @(t,x) [alpha*x(1)*(1-(x(1)/Tc))-h*((lambda_K*x(1)*x(2)+lambday*x(1)*[x(4);x(6);x(7)])/(x(1)+x(2)+x(4)+x(6)+x(7)+1));...
     h*x(5)*x(2)-delta_K*x(2);...
     2^m*sB*x(1)+h*x(5)*x(3)-rE*x(3)-delta_B*x(3);...
-    rE*x(3)-r_t*x(4)-deltas(1)*x(4);...
+    rE*x(3)-r_t*x(4)-deltay(1)*x(4);...
     rho_K*x(2)+rho_B*x(3)-h*x(5)*(x(2)+x(3))-delta_P*x(5);...
-    r_t*x(4)-r_t*x(6)-deltas(2)*x(6);...
-    r_t*x(6)-deltas(3)*x(7)];
+    r_t*x(4)-r_t*x(6)-deltay(2)*x(6);...
+    r_t*x(6)-deltay(3)*x(7)];
 
-x0_E=[1,2*1e-4,0,0,0,0,0];
+x0_E=[3,2*1e-4,0,0,0,0,0];
 
 [~,xHom]=ode23(odefun2,tspan, x0_E);
 
@@ -108,20 +108,20 @@ drawnow;
 % E_1=x(7) Deeply-Exausted T cells                       per k per mm^2
 
 
-lambdas=[lambda_K,lambda_K+0.5*(lambda_E-lambda_K),lambda_E];
-deltas=[delta_K,delta_K+0.5*(delta_E-delta_K),delta_E];
-r_t=5*rE; %FIANDA
+lambday=[lambda_K,lambda_K+0.5*(lambda_E-lambda_K),lambda_E];
+deltay=[delta_K,delta_K+0.5*(delta_E-delta_K),delta_E];
+r_t=5*rE;
 
-odefun2= @(t,x) [alpha*x(1)*(1-(x(1)/Tc))-h*((lambda_K*x(1)*x(2)+lambdas*x(1)*[x(4);x(6);x(7)])/(x(1)+x(2)+x(4)+x(6)+x(7)+1));...
+odefun2= @(t,x) [alpha*x(1)*(1-(x(1)/Tc))-h*((lambda_K*x(1)*x(2)+lambday*x(1)*[x(4);x(6);x(7)])/(x(1)+x(2)+x(4)+x(6)+x(7)+1));...
     h*x(5)*x(2)-delta_K*x(2);...
     2^m*sB*x(1)+h*x(5)*x(3)-rE*x(3)-delta_B*x(3);...
-    rE*x(3)-r_t*x(4)-deltas(1)*x(4);...
+    rE*x(3)-r_t*x(4)-deltay(1)*x(4);...
     rho_K*x(2)+rho_B*x(3)-h*x(5)*(x(2)+x(3))-delta_P*x(5);...
-    r_t*x(4)-r_t*x(6)-deltas(2)*x(6);...
-    r_t*x(6)-deltas(3)*x(7)];
+    r_t*x(4)-r_t*x(6)-deltay(2)*x(6);...
+    r_t*x(6)-deltay(3)*x(7)];
 
 x0triv_E=[0,0,0,0,0,0,0];
-x0_E=[1,2*1e-4,0,0,0,0,0];
+x0_E=[3,2*1e-4,0,0,0,0,0];
 
 [~,xHom]=ode23(odefun2,tspan, x0_E);
 
@@ -138,7 +138,7 @@ subplot(4,2,7); plot(tspan, x(:,5)); title('P'); xlabel('Days'); ylabel('IL-2');
 figure
 subplot(3,2,1); plot(tspan, x(:,4)); title('E'); xlabel('Days'); ylabel('Terminal');
 subplot(3,2,2); plot(tspan, xHom(:,4)); title('E0'); xlabel('Days'); ylabel('Initially exhausted');
-subplot(3,2,3); plot(tspan, xHom(:,4)+xHom(:,6)+xHom(:,7)); title('E = E0 + E1/2 + E1'); xlabel('Days'); ylabel('sum');
+subplot(3,2,3); plot(tspan, xHom(:,4)+xHom(:,6)+xHom(:,7)); title('E0 + E1/2 + E1'); xlabel('Days'); ylabel('sum');
 subplot(3,2,4); plot(tspan, xHom(:,6)); title('E1/2'); xlabel('Days'); ylabel('Middle exhausted');
 subplot(3,2,6); plot(tspan, xHom(:,7)); title('E1'); xlabel('Days'); ylabel('Terminally exhausted');
 drawnow;
@@ -146,3 +146,22 @@ drawnow;
 
 %% Extension of the model - continuous population
 
+lambday=@(y) lambda_K+y*(lambda_E-lambda_K);
+deltay=@(y) delta_K+y*(delta_E-delta_K);
+r_t=@(y) rE*(1+4*y);
+
+
+odefun= @(t,x) [];
+
+x0triv=[0,0,0,0,0];
+x0=[3,2*1e-4,0,0,0];
+
+[t,x]=ode23(odefun,tspan, x0);
+
+figure %no subplot, ma tutti diversi direi
+subplot(5,1,1); plot(tspan, x(:,1)); title('N'); xlabel('Days'); ylabel('Tumor');
+subplot(5,1,2); plot(tspan, x(:,2)); title('K'); xlabel('Days'); ylabel('Preexisting');
+subplot(5,1,3); plot(tspan, x(:,3)); title('B'); xlabel('Days'); ylabel('Progenitor');
+subplot(5,1,4); plot(tspan, x(:,4)); title('E'); xlabel('Days'); ylabel('Terminal');
+subplot(5,1,5); plot(tspan, x(:,5)); title('P'); xlabel('Days'); ylabel('IL-2');
+drawnow;
